6033c85b556b29cf3f8d8eb978819826f5a58fcc
Alan Cornejo <alancornejo@icloud.com>
2017-10-21 19:50:02 -0500

    Se agrega el nombre del proyecto en las llamadas encoladas, se crea la function create.

--- api/controllers/call_waitingController.js
+++ api/controllers/call_waitingController.js
@@ -5,10 +5,30 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 module.exports = {
-  delete: function (req, res) {
+  deleteCallWaiting: function (req, res) {
     let query = {number_phone: req.param('number_phone')}
     call_waiting.destroy(query)
-    .then(data => res.json(data[0].number_phone))
+    .then(data => {
+      const values = {
+        'name_proyect': data[0].name_proyect,
+        'number_phone': data[0].number_phone
+      }
+
+      res.json(values)
+    })
     .catch(err => res.json(err))
+  },
+
+  createCallWaiting: function (req, res) {
+    let values = {
+      number_phone: req.param('number_phone'),
+      name_number: req.param('name_number'),
+      name_queue: req.param('name_queue'),
+      start_call: req.param('start_call'),
+      name_proyect: process.env.nameProyect
+    }
+
+    console.log(values)
+    call_waiting.create(values).then(data => res.json(data)).catch(err => res.json(err))
   }
 }
